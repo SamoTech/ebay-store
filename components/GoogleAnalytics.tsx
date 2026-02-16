621b6a8e37b5ba724e41308192e54c663df51606
Ossama Hashim <samo.hossam@gmail.com>
2026-02-16 03:46:23 +0200

    📊 Add Google Analytics component with gtag.js

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,29 @@
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  const GA_MEASUREMENT_ID = 'G-S5PC9TJ65Z';
+
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
