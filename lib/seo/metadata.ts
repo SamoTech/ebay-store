1b4f012916aeb45d6f4b872fe2bd9739af6932ae
Ossama Hashim <samo.hossam@gmail.com>
2026-09-26 12:47:35 +0300

    fix: use app icon for site metadata

--- lib/seo/metadata.ts
+++ lib/seo/metadata.ts
@@ -100,16 +100,9 @@ export const siteMetadata: Metadata = {
     telephone: false,
   },
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/icon-192x192.png', type: 'image/png', sizes: '192x192' },
-      { url: '/icon-512x512.png', type: 'image/png', sizes: '512x512' },
-    ],
-    apple: [
-      { url: '/apple-icon.png', sizes: '180x180' },
-      { url: '/icon-192x192.png', sizes: '192x192' },
-    ],
-    shortcut: '/favicon.ico',
+    icon: [{ url: '/icon.png', type: 'image/png', sizes: '512x512' }],
+    apple: [{ url: '/icon.png', type: 'image/png', sizes: '512x512' }],
+    shortcut: '/icon.png',
   },
   manifest: '/manifest.json',
 }
