1c814edf1fe1ac07fe9d3d3154c1ba5cc3471aa1
Ossama Hashim <samo.hossam@gmail.com>
2026-09-26 09:38:49 +0300

    seo: explicitly allow search crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,18 @@ import { SITE_URL } from '../lib/site'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/favorites', '/compare'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
   }
